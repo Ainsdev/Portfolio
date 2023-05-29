d577a61c05de88cc75cbe8621180310a61472fee
Ainsdev <85417168+Ainsdev@users.noreply.github.com>
2023-05-29 18:53:43 -0400

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -35,7 +35,7 @@ export async function POST(request: NextRequest) {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
+        private_key: process.env.GOOGLE_PRIVATE_KEY ? process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n") : undefined,
       },
       scopes: [
         "https://www.googleapis.com/auth/drive",
