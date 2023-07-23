7b170b54e8c8f482135e3822f348a76b10d70bd8
Ainsdev <ainsn1654@gmail.com>
2023-07-23 00:09:35 -0400

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import ClipPathTransition from "@/components/widgets/carousel/projects"
 
 const AboutPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center p-16">
       <div className="flex h-1/2 w-2/3 flex-row items-center justify-center overflow-hidden bg-green-500">
         <div className="flex h-max w-max items-center justify-center overflow-x-scroll">
-          <ClipPathTransition />
+          <h1 className="text-9xl text-white">About</h1>
         </div>
       </div>
     </div>
